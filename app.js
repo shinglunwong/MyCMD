be0f203a2bc7d7b7c38a405bc4843e503cf90ea5
shawn137080 <shawn137080@gmail.com>
2018-05-07 16:58:48 +0800

    env and knex

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// require('dotenv').config();
+
+
 
 var app = express();
 
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: process.env.DB_HOST,
+      database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASS
     }
