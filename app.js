8f4cb82ffc6b85829df6bff57142851ad3d40b50
Your Name <you@example.com>
2018-05-08 13:54:07 +0800

    not finish my code yet

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan');// 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');// whats that???
+var usersRouter = require('./routes/users');// whats that???
 
 const knex = require('knex')({
     // Config containing the information of the database connection.
