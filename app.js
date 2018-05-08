630398dd052b613f5ec308050240de89667c5c83
shawn137080 <shawn137080@gmail.com>
2018-05-08 19:03:15 +0800

    passport update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('./passport-init');
+var app = express();
 // var logger = require('morgan');  ?????
 
 // var indexRouter = require('./routes/index');   whats that???
 // var usersRouter = require('./routes/users');   whats that???
+const router = require("./router")(express)
 
 const knex = require('knex')({
     // Config containing the information of the database connection.
@@ -17,17 +20,17 @@ const knex = require('knex')({
 });
 
 
-var app = express();
 
 // app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport(app);
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-
+app.use('/',router);
 app.listen(8080);
 
-module.exports = app;
+// module.exports = app;
