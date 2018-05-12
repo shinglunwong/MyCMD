79e94fcab3a2dde709a34e8ee9c55f48d12bb37b
Your Name <you@example.com>
2018-05-13 01:07:41 +0800

    update part - burn calories

--- router.js
+++ router.js
@@ -7,14 +7,14 @@ module.exports = (express, knex) => {
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
-        } 
+        }
         res.redirect('/');
     }
 
     router.get('/signed', (req, res) => {
         res.send('signed in !');
     });
-    
+
     router.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/signed',
         failureRedirect: '/error'
@@ -23,28 +23,28 @@ module.exports = (express, knex) => {
     router.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     });
-    
-    router.get('/testing',isLoggedIn,(req, res) => {
+
+    router.get('/testing', isLoggedIn, (req, res) => {
         res.sendFile(__dirname + '/testing.html');
     });
 
     router.post('/login', passport.authenticate('local-login', {
         successRedirect: '/dashboard',
         failureRedirect: '/error',
-        successFlash: 'Welcome!' 
+        successFlash: 'Welcome!'
     }));
     //log out
-    router.get('/logout', function(req, res){
+    router.get('/logout', function (req, res) {
         console.log('HI');
         req.logout();
         res.redirect('/');
-      });
+    });
 
     //check food  VVV
     router.post('/api/xxx', (req, res) => {
 
         console.log(req.user); //testing
-         
+
         axios({
             url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
             method: 'post',
@@ -68,37 +68,52 @@ module.exports = (express, knex) => {
     router.post('/api/save-result', (req, res) => {
         console.log(req.user);
         console.log(req.body.calories); //testing
-               knex('get').insert({
-                  record: parseFloat(req.body.calories), user_idkey: req.user.id}).then() //.then() is for insert the result
+        knex('get').insert({
+            record: parseFloat(req.body.calories), user_idkey: req.user.id
+        }).then() //.then() is for insert the result
     });
 
     router.get('/error', (req, res) => {
         res.send('failed');
     });
 
-    router.get('/dashboard', isLoggedIn,(req,res) =>{
+    router.get('/dashboard', isLoggedIn, (req, res) => {
         res.sendFile(__dirname + '/dashboard.html');
     })
-    router.get('/getExe',(req,res) => {
-        knex.select('id','name','MET').from('activity')
+    router.get('/getExe', (req, res) => {
+        knex.select('id', 'name', 'MET').from('activity')
             .then((data) => {
                 res.json(data);
-    
+
             }).catch((err) => {
                 console.log(err)
             })
-        })
-    
-        router.get('/cal',(req,res) => {
-            let user = req.user
-            knex.select('weight').from('user_profile').where({id:user.id})
-                .then((data) => {                    
-                    res.json(data[0].weight);
-                    console.log(data[0].weight);
-        
-                }).catch((err) => {
-                    console.log(err)
-                })
+    })
+
+    router.get('/cal', (req, res) => {
+        let user = req.user
+        knex.select('weight').from('user_profile').where({ id: user.id })
+            .then((data) => {
+                res.json(data[0].weight);
+                console.log(data[0].weight);
+
+            }).catch((err) => {
+                console.log(err)
             })
+    })
+
+    router.post('/api/save-burnresult', (req, res) => {
+        knex('burn').insert({ record: parseFloat(req.body.burncCalories), user_idkey: req.user.id }).then((data) => {
+            res.json({ msg: 'saved calories!' });
+        }).catch((err) => {
+            console.log(err);
+            res.status(409).json({ msg: "cannot save" }) //correct syntax
+        })
+    });
+
+
+    router.get('/api/input-data-chart', (req, res) => {
+        
+    })
     return router;
 };
