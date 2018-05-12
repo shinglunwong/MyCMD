9e0b19037397045ba5b16e84ff0b9747dacacd06
Your Name <you@example.com>
2018-05-12 12:50:34 +0800

    commit

--- router.js
+++ router.js
@@ -7,8 +7,7 @@ module.exports = (express, knex) => {
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
-        }
-
+        } 
         res.redirect('/');
     }
 
