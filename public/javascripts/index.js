bca8f84c7820b5ba6b5cda94b95358c81a2abe77
ShingLun Wong <shinglun.wong@gmail.com>
2018-05-13 11:27:55 +0800

    index html update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,26 @@
-$(function() {
-    $("input[name='password2']").keyup(function() {
+$(function () {
+    $("input[name='password2']").keyup(function () {
         if ($("input[name='password1']").val() == $("input[name='password2']").val()) {
             $("p.validate").html('correct')
         } else {
             $("p.validate").html('not correct')
         }
-      });
+    });
 
-
-});
+    $("div.namecard").click(function () {
+        let index = $('div.namecard').index(this);
+        switch (index) {
+            case 0:
+                $('#target-input').val('shawn137080@gmail.com')
+                break;
+            case 1:
+                $('#target-input').val('rtvk2004@hotmail.com')
+                break;
+            case 2:
+                $('#target-input').val('shinglun.wong@gmail.com')
+                break;
+            default:
+                break;
+        }
+    })
+})
