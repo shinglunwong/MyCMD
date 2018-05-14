22eef505edce7b170eb93ca31d945129a08b5948
shawn137080 <shawn137080@gmail.com>
2018-05-14 17:18:58 +0800

    modified again

--- router.js
+++ router.js
@@ -92,6 +92,7 @@ module.exports = (express, knex) => {
 
     router.get('/cal', (req, res) => {
         let user = req.user
+        console.log('HI')
         console.log(user)
         knex.select('weight').from('user_profile').where({ 'id': user.id })
             .then((data) => {
